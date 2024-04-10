23bd121a5efaed9ce3159bbf48a7a0bb84dcee37
antonellaOvO <125452008+antonellaOvO@users.noreply.github.com>
2024-04-10 18:43:47 +0200

    added -carrito de la compra-project

---
+++ carrito de la compra/README.md
@@ -0,0 +1,27 @@
+# bootstrap-calculator
+
+Instructions
+Utiliza Bootstrap para crear una interfaz de usuario que incluya lo siguiente:
+
+1. Campos de entrada para el total de la factura y el porcentaje de propina.
+
+2. Un botón de "Calcular" para calcular la propina total.
+
+3. Un área de resultado para mostrar el total de la propina calculada.
+
+
+Utiliza JavaScript para agregar funcionalidad de cálculo a la página:
+
+
+
+1. Al hacer clic en el botón "Calcular", debe calcularse el monto total de la propina en función del total de la factura y el porcentaje de propina ingresado.
+
+2. El resultado debe mostrarse en el área de resultado de la página de manera clara y legible.
+
+3. Crea un pop-up o Modal para mostrar el resultado total de la factura con propina y el desglose de ambos gastos.
+
+
+Extra:
+
+Añade la funcionalidad extra de añadir platos / bebidas a la factura y que se calcule el total de esta a partir de los datos introducidos.
+Añade otras funcionalidades extra que creas convenientes, por ejemplo, sistemas de vales y descuentos, colores indicativos, etc.
---
+++ carrito de la compra/src/index.js
@@ -0,0 +1,86 @@
+let form = document.querySelector("form");
+let inputProductoOne = document.getElementById("inputProducto1");
+let inputProductoTwo = document.getElementById("inputProducto2");
+let inputProductoThree = document.getElementById("inputProducto3");
+let inputProductoFour = document.getElementById("inputProducto4");
+let inputProductoFive = document.getElementById("inputProducto5");
+
+let resultTotal = document.querySelector("h3#resultTotal");
+let resultDiscount = document.querySelector("h3#resultDiscount");
+
+/*Esta función submit() es para hacer el cálculo y mostrar el resultado en el HTML elemento*/
+function submit(event) {
+  event.preventDefault();
+  let listaProductos = [
+    Number(inputProductoOne.value),
+    Number(inputProductoTwo.value),
+    Number(inputProductoThree.value),
+    Number(inputProductoFour.value),
+    Number(inputProductoFive.value),
+  ];
+  let sum = 0;
+  function sumar() {
+    for (i = 0; i < listaProductos.length; i++) {
+      sum += listaProductos[i];
+    }
+    let resultado = sum;
+    if (resultado > 100) {
+      let discount = 15;
+      let totalDiscount = (resultado * discount) / 100;
+      let totalWithDiscount = resultado - totalDiscount;
+
+      resultTotal.innerHTML = `PRECIO TOTAL: ${resultado}€`;
+      resultDiscount.innerHTML = `PRECIO CON DESCUENTO: ${totalWithDiscount}€ 👌`;
+      resultDiscount.style.color = "green";
+      let messageBodyModal = document.getElementById("modalMessage");
+      messageBodyModal.textContent = `Wow tu descuento es de ${totalDiscount}€!! Eso es un gran descuento! En total sólo pagarás ${totalWithDiscount}€😉`;
+    } else if (resultado <= 100) {
+      resultTotal.innerHTML = `PRECIO TOTAL: ${resultado}€`;
+      resultDiscount.innerHTML = `NO HAY DESCUENTO ☹`;
+      let messageBodyModal = document.getElementById("modalMessage");
+      messageBodyModal.textContent = `En total sólo pagarás ${resultado}😉`;
+    }
+  }
+
+  sumar();
+
+  /*let total =
+    Number(inputProductoOne.value) +
+    Number(inputProductoTwo.value) +
+    Number(inputProductoThree.value) +
+    Number(inputProductoFour.value) +
+    Number(inputProductoFive.value);
+  let discount = 15;
+  let totalDiscount = (total * discount) / 100;
+
+  let totalWithDiscount = total - totalDiscount;
+
+  resultTotal.innerHTML = `PRECIO TOTAL: ${total}€`;
+  resultDiscount.innerHTML = `PRECIO CON DESCUENTO: ${totalWithDiscount}€ 👌`;
+  let messageBodyModal = document.getElementById("modalMessage");
+  messageBodyModal.textContent = `Wow tu descuento es de ${totalDiscount}€!! Eso es un gran descuento! En total sólo pagarás ${totalWithDiscount}😉`;*/
+}
+
+/*Esta función showModal() es para mostrar el modal*/
+function showModal() {
+  var myModal = new bootstrap.Modal(document.getElementById("myModal"));
+  myModal.show();
+}
+
+/*Este evento es para esconder el modal cuando clicas en el botón "close" (dentro del modal)*/
+document.addEventListener("DOMContentLoaded", function () {
+  var buttonClose = document.querySelector(".btn-close");
+  buttonClose.addEventListener("click", function () {
+    var my_Modal = document.querySelector("#myModal");
+
+    var anotherModal = new bootstrap.Modal(my_Modal);
+
+    anotherModal.hide();
+  });
+});
+
+/*Este evento es para hacer el cálculo (ejecutar la función) cuando clica sobre el botón "submit"*/
+form.addEventListener("submit", submit);
+
+/*Este evento es para mostrar el modal(ejecutar la función) cuando clicas sobre el botón "submit"*/
+form.addEventListener("submit", showModal);
