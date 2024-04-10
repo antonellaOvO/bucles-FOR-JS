51043ec4d321879480768efb04bf29ee1035a64b
antonellaOvO <125452008+antonellaOvO@users.noreply.github.com>
2024-04-10 16:33:32 +0200

    added JS code to show only odd numbers from the inputs that the users enters

--- números pares/src/index.js
+++ números pares/src/index.js
@@ -1,32 +1,29 @@
 function showNumbers(event) {
   event.preventDefault();
   let number = [];
-  for (let i = 50; i >= 20; i -= 1) {
-    let arrayNumber = [i];
-    number = number.concat([i]);
-    console.log(number);
-    let element = document.getElementById("numberTitle");
-    element.textContent = `${number}`;
+  let inputNumberOne = document.getElementById("numeroOne").value;
+  let inputNumberTwo = document.getElementById("numeroTwo").value;
+  console.log(inputNumberOne);
+  if (inputNumberOne > inputNumberTwo) {
+    for (i = inputNumberOne; i >= inputNumberTwo; i--) {
+      if (i % 2 === 0) {
+        number = number.concat([i]);
+        let element = document.getElementById("numberTitle");
+        element.textContent = `${number}`;
+      }
+    }
+  }
+  if (inputNumberOne < inputNumberTwo) {
+    for (i = inputNumberOne; i <= inputNumberTwo; i++) {
+      if (i % 2 === 0) {
+        number = number.concat([i]);
+        console.log(number);
+        let element = document.getElementById("numberTitle");
+        element.textContent = `${number}`;
+        console.log(element);
+      }
+    }
   }
 }
 let button = document.getElementById("button");
 button.addEventListener("click", showNumbers);
-/*let inputNumberOne = document.getElementById("");
-let inputNumberTwo = document.getElementById("");
-if(inputNumberOne > inputNumberTwo) {
-for (i=inputNumberOne; i< inputNumberTwo; i++){
-  let arrayNumber = [i];
-    number = number.concat([i]);
-    console.log(number);
-    let element = document.getElementById("numberTitle");
-    element.textContent = `${number}`;
-};
-} else (inputNumberOne < inputNumberTwo){
-  for (i=inputNumberOne; i< inputNumberTwo; i--){
-  let arrayNumber = [i];
-    number = number.concat([i]);
-    console.log(number);
-    let element = document.getElementById("numberTitle");
-    element.textContent = `${number}`;
-};
-}*/
--- orden descendente/src/index.js
+++ orden descendente/src/index.js
@@ -2,9 +2,7 @@ function showNumbers(event) {
   event.preventDefault();
   let number = [];
   for (let i = 50; i >= 20; i -= 1) {
-    let arrayNumber = [i];
     number = number.concat([i]);
-    console.log(number);
     let element = document.getElementById("numberTitle");
     element.textContent = `${number}`;
   }
